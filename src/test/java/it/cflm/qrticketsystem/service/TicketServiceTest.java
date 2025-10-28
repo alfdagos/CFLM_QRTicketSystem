25b4c18b3eb3552b856cf30cc7629e8e4381265a
Alfredo D'Agostino <alfredo.dagostino@gmail.com>
2025-10-28 08:50:18 +0100

    test: Add MockitoSettings for lenient strictness in TicketServiceTest

--- src/test/java/it/cflm/qrticketsystem/service/TicketServiceTest.java
+++ src/test/java/it/cflm/qrticketsystem/service/TicketServiceTest.java
@@ -14,6 +14,8 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.junit.jupiter.MockitoSettings;
+import org.mockito.quality.Strictness;
 
 import java.util.Optional;
 import java.util.UUID;
@@ -26,6 +28,7 @@ import static org.mockito.Mockito.*;
  * Test unitari per il servizio TicketService.
  */
 @ExtendWith(MockitoExtension.class)
+@MockitoSettings(strictness = Strictness.LENIENT)
 class TicketServiceTest {
 
     @Mock
